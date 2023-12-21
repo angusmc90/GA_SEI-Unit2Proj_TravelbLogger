b1283fa8663cc5288469d7de03559a3a0b3757f4
angusmc90 <angusmc90@gmail.com>
2023-12-20 23:26:45 -0600

    test bouncer function can capture params

--- controllers/bouncer.js
+++ controllers/bouncer.js
@@ -6,5 +6,8 @@ module.exports = function bouncer (req, res, next) {
     console.log(req.originalUrl)
     console.log("Params:")
     console.log(req.params.tripID)
+    res.locals.tripID = req.params.tripID
+    console.log("local variable")
+    console.log(res.locals.tripID)
     next()
   }
