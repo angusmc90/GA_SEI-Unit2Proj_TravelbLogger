233be904c84f34e56e2a4bb50c2b29a05d4a6ae6
angusmc90 <angusmc90@gmail.com>
2023-12-28 15:10:14 -0600

    create show route for excursions

--- controllers/excursions.js
+++ controllers/excursions.js
@@ -4,7 +4,8 @@ const ExcursionModel = require("../models/excursion");
 
 module.exports ={
     new: newExcursion,
-    create
+    create,
+    show,
 }
 
 function newExcursion (req, res){
@@ -39,25 +40,11 @@ async function create (req, res) {
     }
 }
 
-
-
-        // //CREATE ATTRIBUTES IN BODY TO CAPTURE USER INFO FOR UI
-        // const user = req.user
-        // req.body.user = user.name //COME BACK AND CHANGE THIS TO PROFILE NAME LATER
-        // req.body.userPFPic = user.avatar
-        // req.body.userID = user._id
-        // // when ready to add photos - 
-        // // req.body.coverShot = "https://i.imgur.com/wOm0coa.png"
-        
-        // // CONVERT "is fave" CHECKBOX TO BOOLEAN
-        // req.body.favorite = !!req.body.favorite
-        // //CREATE DOC IN DATABASE
-        // const excursionDoc = await ExcursionModel.create(req.body);
-        // // console.log(tripDoc)
-
-        // // we need to add user details so we can see them in the trip and possible link to them
-        // // then we need to push the tripDoc to the user doc
-        // // user id does not need to be in the trips object
-
-        // //GO BACK TO FEED
-        // res.render('trips')
+async function show (req, res) {
+    try {
+        const excursionDocument = await ExcursionModel.findById(req.params.excursionID)
+        res.render("trips//excursions/show", { excursionDoc: excursionDocument })
+    } catch(err) {
+        console.log(err)
+    }   
+}
--- routes/excursions.js
+++ routes/excursions.js
@@ -7,5 +7,6 @@ const assignLocals = require('../controllers/locals')
 // when the path below is called, execute the corresponding method functions defined in the controller
 router.get('/new', excursionsCntrl.new) // BEEF NOTE: new excursion form
 router.post('/create', excursionsCntrl.create) // BEEF NOTE: create new excursion doc
+router.get('/:excursionID', excursionsCntrl.show) // BEEF NOTE: create new excursion doc
 
 module.exports = router;
