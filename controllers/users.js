f828e8d6ad732270d7c76a3d0e52d68bc2b2d05b
angusmc90 <angusmc90@gmail.com>
2023-12-28 15:32:30 -0600

    update users controller to populate the trips document

--- controllers/users.js
+++ controllers/users.js
@@ -7,7 +7,11 @@ module.exports ={
 async function show (req, res){
     console.log(req.user)
     try {
+        // find the user document
         const userDocument = await UserModel.findById(req.params.userID)
+        // populate the trips param so they can render on user page
+        .populate('trips')
+        // render the user profile page
         res.render("users/show", {userDoc: userDocument})
     } catch(err) {
         console.log(err)
