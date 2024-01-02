caa6829a9cd72e8e8fff5f6d7e0897840a9900e6
Angus <34198794+angusmc90@users.noreply.github.com>
2024-01-02 10:29:55 -0600

    added excursion edit route

--- controllers/excursions.js
+++ controllers/excursions.js
@@ -6,6 +6,7 @@ module.exports ={
     new: newExcursion,
     create,
     show,
+    edit,
 }
 
 function newExcursion (req, res){
--- routes/excursions.js
+++ routes/excursions.js
@@ -5,8 +5,9 @@ const assignLocals = require('../controllers/locals')
 
 
 // when the path below is called, execute the corresponding method functions defined in the controller
-router.get('/new', excursionsCntrl.new) // BEEF NOTE: new excursion form
-router.post('/create', excursionsCntrl.create) // BEEF NOTE: create new excursion doc
-router.get('/:excursionID', excursionsCntrl.show) // BEEF NOTE: create new excursion doc
+router.get('/new', assignLocals.isLoggedIn, excursionsCntrl.new) // BEEF NOTE: new excursion form
+router.post('/create', assignLocals.isLoggedIn, excursionsCntrl.create) // BEEF NOTE: create new excursion doc
+router.get('/:excursionID', assignLocals.isLoggedIn, excursionsCntrl.show) // BEEF NOTE: render an excursion page
+router.get('/edit/:excursionID', assignLocals.isLoggedIn, excursionsCntrl.edit) // BEEF NOTE: render a page to edit excursion page
 
 module.exports = router;
