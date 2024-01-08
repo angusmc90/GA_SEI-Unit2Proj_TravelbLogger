aa4a607ad17b9c6ffeb4b661168edec2f8b7d37f
Angus <34198794+angusmc90@users.noreply.github.com>
2024-01-08 16:05:22 -0600

    define edit route

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 const passport = require('passport');
 const userCntrl = require('../controllers/users')
 
-router.get('/:userID', userCntrl.show) // BEEF NOTE: Render a users individual page
+router.get('/:userID', userCntrl.show) // Render a users individual page
+router.get('/:userID/edit', userCntrl.edit) // Render the edit user page
+// NOTE TO SELF - NEED TO REDIRECT HERE IF USER DOES NOT HAVE A PROFILE NAME 
 
 module.exports = router;
