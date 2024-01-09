13cdbd9c58d9661ea0fe363c05c8e474fca99901
Angus <34198794+angusmc90@users.noreply.github.com>
2024-01-09 00:08:14 -0600

    add excursions and trip to user doc for edit page drop-downs

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,5 @@
 const UserModel = require("../models/user");
+const ExcursionModel = require("../models/excursion");
 
 module.exports ={
     show,
@@ -22,8 +23,13 @@ async function show (req, res){
 
 async function edit (req, res) {
     try {
+        const userID = req.params.userID
         // find the user document
-        const userDocument = await UserModel.findById(req.params.userID)
+        const userDocument = await UserModel.findById(userID)
+        .populate('trips')
+        // find excursions the user took
+        const excursions = await ExcursionModel.find({ userID: userID })
+        userDodument.excursions = excursions
         // render the edit page
         res.render("users/edit", { userDoc: userDocument })
     } catch(err) {
