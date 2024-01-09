d9adcc56c4af1407cc31a45ef5d5d6edb4f9d032
Angus <34198794+angusmc90@users.noreply.github.com>
2024-01-09 12:05:10 -0600

    final changes

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,10 @@ const router = require('express').Router();
 const passport = require('passport');
 const userCntrl = require('../controllers/users')
 const assignLocals = require('../controllers/locals')
-// , assignLocals.isLoggedIn,
 
-router.get('/:userID', userCntrl.show) // Render a users individual page
-router.get('/:userID/edit', userCntrl.edit) // Render the edit user page
+router.get('/:userID', assignLocals.isLoggedIn, userCntrl.show) // Render a users individual page
+router.get('/:userID/edit', assignLocals.isLoggedIn, userCntrl.edit) // Render the edit user page
 // NOTE TO SELF - NEED TO REDIRECT HERE IF USER DOES NOT HAVE A PROFILE NAME 
-router.post('/:userID', userCntrl.update) // Update Mongo Doc & redirect
+router.post('/:userID', assignLocals.isLoggedIn, userCntrl.update) // Update Mongo Doc & redirect
 
 module.exports = router;
