64aa7836650e9112a92b8198acc8de9c143845ac
Angus <34198794+angusmc90@users.noreply.github.com>
2024-01-09 11:28:41 -0600

    define update route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
 const passport = require('passport');
 const userCntrl = require('../controllers/users')
+const assignLocals = require('../controllers/locals')
+// , assignLocals.isLoggedIn,
 
 router.get('/:userID', userCntrl.show) // Render a users individual page
 router.get('/:userID/edit', userCntrl.edit) // Render the edit user page
 // NOTE TO SELF - NEED TO REDIRECT HERE IF USER DOES NOT HAVE A PROFILE NAME 
+router.post('/:userID', userCntrl.update) // Update Mongo Doc & redirect
 
 module.exports = router;
